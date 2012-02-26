3c42581fb0b7890bd05bb56f4c574f6fb7f8efb6
unknown <wildbillcat@.(none)>
2012-02-25 22:05:20 -0500

    Deleted Ferpa controllers, updated form for the submissions.

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -3,18 +3,12 @@ authorization do
     has_permission_on [:admin_users, :users, :workstations, :equip_agreements, :laser_agreements, :print_agreements, :products,
        :transactions, :directory, :submissions, :holidays ], 
       :to => [:index, :show, :new, :create, :edit, :update, :destroy, :edit_multiple, :update_multiple, :checkin, :print_multiple, :advanced_search]  
-    has_permission_on [:ferpa], :to => [:agreement, :update]
-    has_permission_on [:ferpa_agreement], :to => [:agreement, :update]
-    has_permission_on [:FerpaAgreement], :to => [:agreement, :update]
   end 
   
   role :dmstaff do  
      has_permission_on [:admin_users, :users, :workstations, :equip_agreements, :laser_agreements, :print_agreements, :products, 
        :transactions, :directory, :submissions ], 
        :to => [:index, :show, :new, :create, :edit, :update, :edit_multiple, :update_multiple, :checkin ]  
-    has_permission_on [:ferpa], :to => [:agreement, :update]
-    has_permission_on [:ferpa_agreement], :to => [:agreement, :update]
-    has_permission_on [:FerpaAgreement], :to => [:agreement, :update]
   end
   
   role :staff do
@@ -40,18 +34,12 @@ authorization do
     has_permission_on [:submissions], :to => [:show ] do
       if_attribute :user_id => is {user.id}  
     end
-    has_permission_on [:ferpa], :to => [:agreement, :update]
-    has_permission_on [:ferpa_agreement], :to => [:agreement, :update]
-    has_permission_on [:FerpaAgreement], :to => [:agreement, :update]
     has_permission_on [:products], :to => [:index, :show ] 
   end
   
   role :faculty do
     has_permission_on [:users, :directory], :to => [:index, :show, :print_multiple ]   
     has_permission_on [:submissions], :to => [:index, :show ]
-    has_permission_on [:ferpa], :to => [:agreement, :update]
-    has_permission_on [:ferpa_agreement], :to => [:agreement, :update]
-    has_permission_on [:FerpaAgreement], :to => [:agreement, :update]
   end
   
 end  
--- config/routes.rb
+++ config/routes.rb
@@ -55,8 +55,6 @@ Students::Application.routes.draw do
   match "/registration" => "registration#index", :as => :registration
   match "/register" => "registration#index", :as => :register
   match "/directory" => "directory#index", :as => :directory
-  get "ferpa_agreement/agreement"
-  post "ferpa_agreement/update"
   #match "/ferpa" => "ferpa_agreement#agreement", :as => :ferpa
   #match "/ferpa/agree" => "ferpa_agreement#update", :as => "agree_ferpa"
   root :to => "directory#index"  
