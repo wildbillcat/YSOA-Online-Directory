fdd69ad07a1a5e72a4ef438db5a01e07d902fec2
unknown <wildbillcat@.(none)>
2012-02-27 20:17:10 -0500

    Updated config for debugging and modified the user model for checking ferpa.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,8 +7,8 @@ Students::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   
-  #config.consider_all_requests_local       = true #dev
-  #config.action_view.debug_rjs             = true #dev
+  config.consider_all_requests_local       = true #dev
+  config.action_view.debug_rjs             = true #dev
   #config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
