875142e29e2bf9dfb3a22696d04a0cba525335af
unknown <pem4@.yu.yale.edu>
2012-08-08 16:47:24 -0400

    Updated Migration for Second Monitor and Arm. Updated HTML Interfaces to reflect the New items.

--- db/migrate/20120808192751_add_second_monitor_and_arm.rb
+++ db/migrate/20120808192751_add_second_monitor_and_arm.rb
@@ -1,7 +1,11 @@
 class AddSecondMonitorAndArm < ActiveRecord::Migration
   def self.up
+    add_column :workstations, :monitor_number2, :string
+    add_column :workstations, :arm_number2, :string
   end
 
   def self.down
+    remove_column :workstations, :monitor_number2
+    remove_column :workstations, :arm_number2
   end
 end
