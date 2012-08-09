8db050f3c20588e9c7098ec44a9b326d2cf8898a
unknown <pem4@.yu.yale.edu>
2012-08-09 11:47:14 -0400

    Tried HTML Escape for the Dashes

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -16,7 +16,7 @@ class Submission < ActiveRecord::Base
   validates_attachment_presence :portfolio
   validates_attachment_size :portfolio, :less_than => 50.megabytes
   
-  COLLECTIONS = ["Design Review: MArch1 \- 2012\-2013", "Design Review: MArch2 \- 2012\-2013", "Graduating Review: MArch1 \- 2012\-2013", "Graduating Review: MArch 2 \- 2012\-2013"]
+  COLLECTIONS = ["Design Review: MArch1 &#8212; 2012&#8212;2013", "Design Review: MArch2 &#8212; 2012&#8212;2013", "Graduating Review: MArch1 &#8212; 2012&#8212;2013", "Graduating Review: MArch 2 &#8212; 2012&#8212;2013"]
 
   def owner
     if user_id && User.exists?(user_id)
