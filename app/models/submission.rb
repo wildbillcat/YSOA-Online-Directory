950dd75b64464f64b8f693e84b7ea93e965f627a
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-08-27 18:24:21 -0400

    Updated submission model for the system for new year

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -16,8 +16,8 @@ class Submission < ActiveRecord::Base
   validates_attachment_presence :portfolio
   validates_attachment_size :portfolio, :less_than => 50.megabytes
   
-  COLLECTIONS = ['Design Review: MArch1 2013 to 2014', 'Design Review: MArch2 2013 to 2014', 'RAMSA Travel Fellowship',
-     '250a Junior Studio Portfolio Review Spring 2014', 'KPF Traveling Fellowship 2014', 'Graduating Students Portfolio Submission May 2014']
+  COLLECTIONS = ['Design Review: MArch1 2014 to 2015', 'Design Review: MArch2 2014 to 2015',
+     'Graduating Students Portfolio Submission December 2014']
 
   def owner
     if user_id && User.exists?(user_id)
